fab06c8a5911f22b788901e42dfddc9c20facadd
葛东亚 <gdy_work_9464@163.com>
2024-01-16 09:09:23 +0800

    init case 修饰符容器等

---
+++ entry/src/main/ets/pages/Index.js
@@ -0,0 +1,29 @@
+"use strict";
+struct Index extends   {
+    constructor() { }
+    build() {
+        // Column 的子组件都会以纵向的方式排列 Row 的子组件左右方式排列
+        ;
+    }
+}
+// comp
+struct Header extends   {
+    constructor() { }
+    build() {
+    }
+}
+struct Main extends   {
+    constructor() { }
+    build() {
+    }
+}
+struct Footer extends   {
+    constructor() { }
+    build() {
+    }
+}
+// @Entry修饰符 @Component自定义组件 @State响应式变量声明
+// struct结构的意思 声明的内容不能与系统的组件名重名
+// struct上面必须有@Component或者@CustomDialog装饰器装饰 声明是组件还是弹窗
+// Column容器组件 - 列
+//# sourceMappingURL=Index.js.map
